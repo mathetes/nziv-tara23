702f60f467066c43dd29f0571c42d37d80b5ee21
mathetes <nsk.roman@gmail.com>
2023-03-31 16:59:46 +0700

    initial state

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import {$,jQuery} from "jquery";
+//import popper from "popper.js";
+import bootstrap from "bootstrap";
+
+import Inputmask from "inputmask";
+//var Inputmask = require('inputmask');
+
+//jQuery(function() {
+//  jQuery("body").css("color", "blue");
+//});
